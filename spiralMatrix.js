01c33dcf71dd66baefe31a2501154883a407956c
Dmitry <sidelevdima@yandex.ua>
2018-10-22 08:09:00 +0300

    add oop style

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -1,38 +1,44 @@
-var cur_i = 0, cur_j = 0;
-var dim_M = 6, dim_N = 6;
-var left = 0, right = dim_N - 1;
-var top = 0, bottom = dim_M - 1;
-var current_dir = 'EAST';
-function fillMatrix()
-{
-    switch (current_dir){
-        case 'EAST' :
-            if (cur_i < right) {
-                cur_i++;
-            } else if (cur_i == right && cur_j < bottom) {
-                cur_j++;
-            } else {
-                current_dir = 'SOUTH'
-                if (right != dim_N - 1 && bottom != dim_M - 1 ) {left++;}
-                top++;
-            }
-            return {'cur_i': cur_i, 'cur_j': cur_j}
-        case 'SOUTH' :
-            if (cur_i > left) {
-                cur_i--;
-            }else if (cur_i == left && cur_j > top) {
-                cur_j--;
-            } else {
-                current_dir = 'EAST';
-                right--;
-                bottom--;
-            }
-            return {'cur_i': cur_i, 'cur_j': cur_j}
-    }
+function SpiralMatrix(n, m) {
+    this.dim_N = n;
+    this.dim_M = m;
+    this.cur_i = 0, this.cur_j = 0;
+    this.left = 0, this.right = this.dim_N - 1;
+    this.top = 0, this.bottom = this.dim_M - 1;
+    this.current_dir = 'e';
+    this.fillMatrix = function() {
+        switch (this.current_dir){
+            case 'e' :
+                if (this.cur_i < this.right) {
+                    this.cur_i++;
+                } else if (this.cur_i == this.right && this.cur_j < this.bottom) {
+                    this.cur_j++;
+                } else {
+                    this.current_dir = 's'
+                    if (this.right != this.dim_N - 1 && this.bottom != this.dim_M - 1 ) {this.left++;}
+                        this.cur_i--;
+                        this.top++;
+                }
+                return {'cur_i': this.cur_i, 'cur_j': this.cur_j}
+            case 's' :
+                if (this.cur_i > this.left) {
+                    this.cur_i--;
+                } else if (this.cur_i == this.left && this.cur_j > this.top) {
+                    this.cur_j--;
+                } else {
+                    this.current_dir = 'e';
+                    this.cur_j--;
+                    this.right--;
+                    this.bottom--;
+                }
+                return {'cur_i': this.cur_i, 'cur_j': this.cur_j}
+        }
+    };
 }
 
+var sp1 = new SpiralMatrix(10, 10);
 var it = 0;
-while (it <= dim_M * dim_N) {
-    console.log(fillMatrix());
-    it++;
+while(it < sp1.dim_N * sp1.dim_M) {
+    it++;  
+    var indexses = sp1.fillMatrix();
+    console.log(indexses);  
 }
